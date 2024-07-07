353989b149ffed90ca7e3c8f7fd9adca23065874
krk224 <hihas93@naver.com>
2024-07-07 12:39:59 +0900

    [feat] 필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,36 +1,50 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
-//@Table(name = "User")
+@Table()
 public class Member {
     @Id
     private Long id;
-//    @Column(name = "username")
-    private String name;
+    @Column(name = "name", nullable = false)
+    private String username;
+    private Integer age;
+
+    @Column(precision = 20, scale=10)
+    private BigDecimal salary;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description; // Lob인데 변수 타입이 String이기 때문에 CLOB으로 생성
+
+    @Transient
+    private int temp;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
 
     public Member() {}
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
