59ab0d19e026c3d4d208347980189295b522e96c
krk224 <hihas93@naver.com>
2024-07-15 11:35:42 +0900

    [feat] JPA getReference 메소드 속성 확인 3. Proxy 객체의 초기화에는 영속성 컨텍스트가 관여한다. 따라서, 영속성 컨텍스트에서 관리하지 않는다면(em.close, em.detach, em.clear) 예외가 발생 - LazyInitializationException

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,7 @@ package hellojpa;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.hibernate.Hibernate;
 
 public class JpaMain {
 
@@ -17,6 +18,25 @@ public class JpaMain {
 
         try {//code
 
+            Member member = new Member();
+            member.setUsername("user");
+            member.setCreatedBy("Kim");
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member refMember = em.getReference(Member.class, member.getId());
+
+            boolean loaded = emf.getPersistenceUnitUtil().isLoaded(refMember);
+            System.out.println("loaded = " + loaded); // false
+            Hibernate.initialize(refMember); // Hibernate에만 존재하는 초기화 메서드
+//            refMember.getUsername(); // 강제 로딩, 모든 JPA에서 사용하는 방법
+            loaded = emf.getPersistenceUnitUtil().isLoaded(refMember);
+            System.out.println("loaded = " + loaded); // true
+
+            em.clear();
+
             Member member1 = new Member();
             member1.setUsername("member1");
             member1.setCreateDate(LocalDateTime.now());
@@ -24,49 +44,62 @@ public class JpaMain {
 
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setCreatedBy("Kim");
-
-            em.persist(member2);
-
             em.flush();
             em.clear();
 
-            Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.find(Member.class, member2.getId());
-            System.out.println("m1 == m2 : " + (m1.getClass()==m2.getClass())); // true;
-            Member reference = em.getReference(Member.class, member2.getId());
-            // 이미 m2.getId에 해당하는 Class가 영속성 컨텍스트에 존재한다. 따라서 Member.class의 인스턴스를 가져온다.
-            System.out.println("m1.getClass() == reference.getClass(): " + (m1.getClass()==reference.getClass())); // true
+            Member refMember2 = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember2.getClass() = " + refMember2.getClass());   // Proxy
 
-            // em.clear 해야지 다시 가져온다...
-            em.clear();
+            em.detach(refMember2); // ?? Proxy 객체를 더 이상 관리하지 않는다. LazyInitializationException: could not initialize proxy
+//            em.close();
+//            em.clear();
 
-            reference = em.getReference(Member.class, member2.getId());
-            System.out.println("m1 == reference: " + (m1.getClass()==reference.getClass())); // false
+            // 실제 DB 쿼리가 발생해서 초기화 진행. => 하지만 더 이상 관리하지 않기 때문에 exception 발생.
+            System.out.println("refMember2.getUsername() = " + refMember2.getUsername());
 
-            logic(m1, reference);
-
-            // em.find
-//            Member findMember = em.find(Member.class, member.getId());
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getUsername() = " + findMember.getUsername());
-
-            // em.getReference
-            Member findMemberReference = em.getReference(Member.class, member1.getId());
-            System.out.println("before findMemberReference.getClass() = " + findMemberReference.getClass());
-            System.out.println("findMemberReference.getId() = " + findMemberReference.getId());
-            // 여기 위까지 DB에 쿼리가 나가지 않는다. Hibernate에서 Proxy객체의 target을 이때 채워준다.
-            System.out.println("findMemberReference.getUsername() = " + findMemberReference.getUsername());
-            System.out.println("findMemberReference.getUsername() = " + findMemberReference.getUsername());
-
-            // 초기화 한다고 해서 Proxy 객체가 기존 Entity로 바뀌지는 않는다.
-            System.out.println("after findMemberReference.getClass() = " + findMemberReference.getClass());
-            // 이것도 마찬가지로 find로 가져와도 Proxy객체를 반환. 영속성 컨텍스트 안에서 같은 PK인 경우 동일함을 보장하기 위해..
-            // find 메소드를 호출하면 DB 조회해서 실제 Entity를 생성하긴 함.
-            Member findMemberReference2 = em.find(Member.class, member1.getId());
-            System.out.println("findMemberReference1 == findMemberReference2: " + (findMemberReference == findMemberReference2));
+//            Member member2 = new Member();
+//            member2.setUsername("member2");
+//            member2.setCreatedBy("Kim");
+//
+//            em.persist(member2);
+//
+//            em.flush();
+//            em.clear();
+//
+//            Member m1 = em.find(Member.class, member1.getId());
+//            Member m2 = em.find(Member.class, member2.getId());
+//            System.out.println("m1 == m2 : " + (m1.getClass()==m2.getClass())); // true;
+//            Member reference = em.getReference(Member.class, member2.getId());
+//            // 이미 m2.getId에 해당하는 Class가 영속성 컨텍스트에 존재한다. 따라서 Member.class의 인스턴스를 가져온다.
+//            System.out.println("m1.getClass() == reference.getClass(): " + (m1.getClass()==reference.getClass())); // true
+//
+//            // em.clear 해야지 다시 가져온다...
+//            em.clear();
+//
+//            reference = em.getReference(Member.class, member2.getId());
+//            System.out.println("m1 == reference: " + (m1.getClass()==reference.getClass())); // false
+//
+//            logic(m1, reference);
+//
+//            // em.find
+////            Member findMember = em.find(Member.class, member.getId());
+////            System.out.println("findMember.getId() = " + findMember.getId());
+////            System.out.println("findMember.getUsername() = " + findMember.getUsername());
+//
+//            // em.getReference
+//            Member findMemberReference = em.getReference(Member.class, member1.getId());
+//            System.out.println("before findMemberReference.getClass() = " + findMemberReference.getClass());
+//            System.out.println("findMemberReference.getId() = " + findMemberReference.getId());
+//            // 여기 위까지 DB에 쿼리가 나가지 않는다. Hibernate에서 Proxy객체의 target을 이때 채워준다.
+//            System.out.println("findMemberReference.getUsername() = " + findMemberReference.getUsername());
+//            System.out.println("findMemberReference.getUsername() = " + findMemberReference.getUsername());
+//
+//            // 초기화 한다고 해서 Proxy 객체가 기존 Entity로 바뀌지는 않는다.
+//            System.out.println("after findMemberReference.getClass() = " + findMemberReference.getClass());
+//            // 이것도 마찬가지로 find로 가져와도 Proxy객체를 반환. 영속성 컨텍스트 안에서 같은 PK인 경우 동일함을 보장하기 위해..
+//            // find 메소드를 호출하면 DB 조회해서 실제 Entity를 생성하긴 함.
+//            Member findMemberReference2 = em.find(Member.class, member1.getId());
+//            System.out.println("findMemberReference1 == findMemberReference2: " + (findMemberReference == findMemberReference2));
 
 
             tx.commit();
