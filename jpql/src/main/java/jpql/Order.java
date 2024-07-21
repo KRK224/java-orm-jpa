9466a754a3b9a49cba130e3491b1bd2d0bd95660
krk224 <hihas93@naver.com>
2024-07-21 12:28:39 +0900

    [feat] JPQL 기본 쿼리

---
+++ jpql/src/main/java/jpql/Order.java
@@ -0,0 +1,25 @@
+package jpql;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "ORDERS")
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int orderAmount;
+
+    @Embedded
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+}
