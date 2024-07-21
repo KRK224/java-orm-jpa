5208421e7da4c102e1a03d35654152fa5d9cc7c3
krk224 <hihas93@naver.com>
2024-07-21 18:21:33 +0900

    [feat] pagination

---
+++ jpql/src/main/java/jpql/PaginationMain.java
@@ -0,0 +1,39 @@
+package jpql;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import java.util.List;
+
+
+public class PaginationMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            for(int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class).setFirstResult(9).setMaxResults(19).getResultList();
+            for (Member member : resultList) {
+                System.out.println("member = " + member);
+            }
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
