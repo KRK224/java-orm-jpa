2e0e6edf0b4448bb8c486897512779c30fb95719
krk224 <hihas93@naver.com>
2024-08-05 11:56:15 +0900

    [feat] fetch join의 한계 1. alias를 걸지 말자 => fetch join의 의미는 객체 그래프 탐색에 있다. 2. pagination이 불가능하다 => 마찬가지로 객체 그래프 탐색에 위반한다. => default_batch_fetch_size를 사용하자. 3. 둘 이상의 컬렉션은 페치 조인 할 수 없다. => 가뜩이나 1대다이면 데이터 뻥튀기인데.. 곱하기 x 곱하기 부작용 발생

--- jpql/src/main/java/jpql/FetchJoinMain.java
+++ jpql/src/main/java/jpql/FetchJoinMain.java
@@ -68,6 +68,7 @@ public class FetchJoinMain {
                 }
 
                 em.clear();
+                // Collection fetch join
                 // DB join 결과를 생각해보면 멤버 2명이 각각 하나의 로우로 나오고 이를 JPA가 취합을 하는데 2개에 대한 처리를 하기 때문.
                 // SQL 상에서는 distinct 해도 아무 소용 없음 -> join한 member의 정보가 다르기 때문
                 // 그런데, JPQL fetch join을 사용한 경우, 동일한 엔티티에 대해서의 중복 제거를 애플리케이션 차원에서 해준다.
@@ -87,15 +88,58 @@ public class FetchJoinMain {
                  *
                  * 패치 조인은 즉시 로딩처럼 처리되며, 객체 그래프를 SQL 한번에 조회하는 개념!!
                  */
-                String generalJoinQuery = "select t from Team t join t.members m";
-                List<Team> teamListWithPlainJoin = em.createQuery(generalJoinQuery, Team.class).getResultList();
-                for (Team team : teamListWithPlainJoin) {
-                    // 여기까지는 Member id만 가진 Proxy객체를 가지고 있어 size 나옴?
-                    // => 아니네... 뭔가 collection 이라서 다른 거 같은데... Member 객체 자체를 나중에 쿼리를 통해 가지고 옴
-                    System.out.println("team: " + team.getName() + " has member: " + team.getMembers().size());
+//                String generalJoinQuery = "select t from Team t join t.members m";
+//                List<Team> teamListWithPlainJoin = em.createQuery(generalJoinQuery, Team.class).getResultList();
+//                for (Team team : teamListWithPlainJoin) {
+//                    // 여기까지는 Member id만 가진 Proxy객체를 가지고 있어 size 나옴?
+//                    // => 아니네... 뭔가 collection 이라서 다른 거 같은데... Member 객체 자체를 나중에 쿼리를 통해 가지고 옴
+//                    System.out.println("team: " + team.getName() + " has member: " + team.getMembers().size());
+//                    for (Member member : team.getMembers()) {
+//                        // 영속성 컨텍스트에 퍼올려지지 않아서 쿼리가 나감
+//                        System.out.println("  --> member = " + member.getUsername());
+//                    }
+//                }
+
+                em.clear();
+
+                String fetchQueryForCollections1 = "select t from Team t join fetch t.members";
+                // collection fetch join을 pagination 한다?
+                // 1대다 관계에서는 DB 데이터가 뻥튀기 된다.. 그런데 pagination으로 자른다는건 데이터 정합성에 어긋남
+                // teamA -> memberA
+                // ---------------- 여기서 잘린다는 의미 => 따라서 JPA에서는 pagination 쿼리를 무시하고 메모리에서 페이징
+                // teamA -> memberB
+                // **WARN: HHH90003004: firstResult/maxResults specified with collection fetch; applying in memory**
+                // 메모리에서 페이징하기 때문에 쿼리로는 데이터가 많아지면 다 올라온다..
+
+//                List<Team> fetchQueryForCollectionsResultWithPageNation = em.createQuery(fetchQueryForCollections1, Team.class)
+//                        .setFirstResult(0)
+//                        .setMaxResults(1)
+//                        .getResultList();
+//                for (Team team : fetchQueryForCollectionsResultWithPageNation) {
+//                    System.out.print("team: " + team.getName() + " has member: " + team.getMembers().size());
+//                    for (Member member : team.getMembers()) {
+//                        System.out.println(" -> member = " + member);
+//                    }
+//                }
+
+                em.clear();
+
+                /**
+                 * 그렇다고 fetch join을 풀자니, N+1 문제가 걸린다.. 어떻게 해야할까?
+                 * hibernate.default_batch_fetch_size 옵션을 걸고, lazy loading을 적용하면 된다.
+                 * => for문에서 N번 쿼리를 날리는게 아닌 fetch_size만큼 in 절을 날려 쿼리를 최적화한다.
+                 * <property name="hibernate.default_batch_fetch_size" value="100"/>
+                 */
+
+                String defaultBatchFetchSizeTest = "select t from Team t";
+                List<Team> defaultBatchFetchSizeTestResult = em.createQuery(defaultBatchFetchSizeTest, Team.class)
+                        .setFirstResult(0)
+                        .setMaxResults(3)
+                        .getResultList();
+                for (Team team : defaultBatchFetchSizeTestResult) {
+                    System.out.print("team: " + team.getName() + " has member: " + team.getMembers().size() + "\n");
                     for (Member member : team.getMembers()) {
-                        // 영속성 컨텍스트에 퍼올려지지 않아서 쿼리가 나감
-                        System.out.println("  --> member = " + member.getUsername());
+                        System.out.println(" -> member = " + member);
                     }
                 }
 
